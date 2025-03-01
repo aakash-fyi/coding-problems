0d64c24aad16c21a13ed3c6aa9090d6588552a40
Aakash Khatkar <aakash@Aakashs-MacBook-Pro-5.local>
2025-03-01 17:42:54 +0530

    add flat nested array

---
+++ 30-day-JS/2625-flat-nested-array.js
@@ -0,0 +1,23 @@
+
+// Given a multi-dimensional array arr and a depth n, return a flattened version of that array.
+
+// A multi-dimensional array is a recursive data structure that contains integers or other multi-dimensional arrays.
+
+// A flattened array is a version of that array with some or all of the sub-arrays removed and replaced with the actual elements in that sub-array. This flattening operation should only be done if the current depth of nesting is less than n. The depth of the elements in the first array are considered to be 0.
+
+// Please solve it without the built-in Array.flat method.
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n, res = []) {
+    for (let i = 0; i < arr.length; i++) {
+        if ((Array.isArray(arr[i]) && n)) {
+            flat(arr[i], n - 1, res)
+        } else {
+            res.push(arr[i])
+        }
+    }
+    return res
+};

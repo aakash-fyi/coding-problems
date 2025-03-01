f374b815a48cf79bf19bbed3f01353aea99e8840
Aakash Khatkar <aakash@Aakashs-MacBook-Pro-5.local>
2025-03-01 10:28:03 +0530

    add group by array

---
+++ 30-day-JS/2631-groupby-array.js
@@ -0,0 +1,25 @@
+// Write code that enhances all arrays such that you can call the array.groupBy(fn) method on any array and it will return a grouped version of the array.
+
+// A grouped array is an object where each key is the output of fn(arr[i]) and each value is an array containing all items in the original array which generate that key.
+
+// The provided callback fn will accept an item in the array and return a string key.
+
+// The order of each value list should be the order the items appear in the array. Any order of keys is acceptable.
+
+// Please solve it without lodash's _.groupBy function.
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    let returnObj = {}
+    for (let i =0; i < this.length; i++){
+        let result = fn(this[i])
+        returnObj[result]? returnObj[result].push(this[i]) : returnObj[result] = [this[i]]
+    }
+    return returnObj
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */

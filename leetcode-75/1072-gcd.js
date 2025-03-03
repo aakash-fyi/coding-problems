db5e3778ac9d7febc3c7c22393b86f39c7495a6a
Aakash Khatkar <aakash@Aakashs-MacBook-Pro-5.local>
2025-03-03 17:22:08 +0530

    add gcd

---
+++ leetcode-75/1072-gcd.js
@@ -0,0 +1,21 @@
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if ( str1 + str2 !== str2 + str1){
+        return ''
+    }
+    let a = str1.length 
+    let b = str2.length 
+    while(b){
+    let temp = b 
+    b = a % b  
+    a = temp
+    }
+    return str1.substring(0, a)
+};
